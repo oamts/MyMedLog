d73e88ed3ff638c76e7881a3a682f018cb110915
Mateus <mateusoliveiraalmeida@gmail.com>
2025-07-08 20:44:17 -0300

    adding medicines

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,23 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-      <Stack
-          screenOptions={{
-              headerStyle: {
-                  backgroundColor: '#444',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                  fontWeight: 'bold',
-              },
-          }}>
-          <Stack.Screen name="index"
-                        options={{
-            headerTitle: () => null,
-          }}/>
-      </Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#003366",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: () => null,
+        }}
+      />
+    </Stack>
   );
 }
