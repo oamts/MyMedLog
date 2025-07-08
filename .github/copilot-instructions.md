b1af106cce43e569dc8da451d3ad2a5a8fcb2eeb
Mateus <mateusoliveiraalmeida@gmail.com>
2025-07-08 18:11:32 -0300

    some changes

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,2 @@
+This is a React native app using Expo. It is created with `create-expo-app` and uses file-based routing. The app is
+designed to be a starting point for building universal apps that run on Android, iOS.
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack
+          screenOptions={{
+              headerStyle: {
+                  backgroundColor: '#444',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                  fontWeight: 'bold',
+              },
+          }}>
+          <Stack.Screen name="index"
+                        options={{
+            headerTitle: () => null,
+          }}/>
+      </Stack>
+  );
 }
