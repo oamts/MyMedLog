7bc74c2f1637e5759ce5c30085537a4050928709
Mateus <mateusoliveiraalmeida@gmail.com>
2026-01-22 21:46:06 -0300

    delete

--- src/redux/selectors.ts
+++
@@ -1,3 +0,0 @@
-import { RootState } from "@/src/redux/store";
-
-export const selectMedicines = (state: RootState) => state.medicine.medicines;
--- src/views/Home/types.ts
+++
@@ -1,5 +0,0 @@
-export type Medicine = {
-  id: string;
-  name: string;
-  expiry: string;
-};
